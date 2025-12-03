eb2bf0e86c64497fd54cc5c353aae4403004b467
Panagiota4 <p.georgari@logicea.com>
2025-12-03 14:02:39 +0200

    create this class to check connection sql--> spring

--- src/main/java/com/logicea/cards/DemoApplicationConnection.java
+++ src/main/java/com/logicea/cards/DemoApplicationConnection.java
@@ -1,29 +1,28 @@
 package com.logicea.cards;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.logicea.cards.CardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// new main class for checking connection sql->spring
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner { // Υλοποιούμε το interface
+public class DemoApplicationConnection implements CommandLineRunner {
 
-    // Χρησιμοποιούμε το Autowired για να "ενέση" (inject) το repository μας
-    @Autowired
+
+    @Autowired // inject to my repository
     private CardRepository cardRepository;
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplicationConnection.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        // ΕΔΩ ΓΙΝΕΤΑΙ Ο ΕΛΕΓΧΟΣ
-        System.out.println("--- ΕΛΕΓΧΟΣ ΣΥΝΔΕΣΗΣ ΒΑΣΗΣ ΔΕΔΟΜΕΝΩΝ ---");
-        long cardCount = cardRepository.count(); // Εκτελείται SQL: SELECT COUNT(*) FROM cards;
-        System.out.println("Βρέθηκαν " + cardCount + " κάρτες στον πίνακα cards.");
-        System.out.println("--- Ο ΕΛΕΓΧΟΣ ΟΛΟΚΛΗΡΩΘΗΚΕ ---");
+
+        System.out.println("--- CHECK CONNECTION SQL -> SPRING ---");
+        long cardCount = cardRepository.count(); // run internal SQL: SELECT COUNT(*) FROM cards;
+        System.out.println("Found" + cardCount + " cards");
+        System.out.println("--------------------------------------");
     }
 }
