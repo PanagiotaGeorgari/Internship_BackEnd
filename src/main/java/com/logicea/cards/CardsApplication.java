0994c673646a8a2182d25b6602843fc31e3be738
Panagiota4 <p.georgari@logicea.com>
2025-12-03 14:04:26 +0200

    put comments

--- src/main/java/com/logicea/cards/CardsApplication.java
+++ src/main/java/com/logicea/cards/CardsApplication.java
@@ -1,5 +1,5 @@
 package com.logicea.cards;
-// 3/12 i put comments in order to run the DemoApplicationConnection for check the connection with sql 
+// 3/12 i put comments in order to run the DemoApplicationConnection for check the connection with sql
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
