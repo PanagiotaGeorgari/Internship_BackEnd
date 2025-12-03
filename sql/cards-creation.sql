182660002766fbe8ea41d7cddc964eb800ab8348
Panagiota4 <p.georgari@logicea.com>
2025-12-03 13:37:36 +0200

    sql queries (creation and insert)

--- sql/cards-creation.sql
+++ sql/cards-creation.sql
@@ -1,3 +1,4 @@
+--create the table cards--
 create table cards(
     card_id int auto_increment primary key,
     name varchar(50) not null,
@@ -6,3 +7,7 @@ create table cards(
     status varchar(50) not null default "Todo",
     user_id int
 );
+
+--insert an item to the table --
+insert into cards(name,description,color,user_id)
+values("first task","connect sql base with spring","red",1);
