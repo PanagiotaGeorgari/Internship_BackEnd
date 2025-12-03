406d664409f8ad07e562cbf4904d8ea1ad3da98d
Panagiota4 <p.georgari@logicea.com>
2025-12-03 13:56:11 +0200

    create the card repository to communicate with base

--- src/main/java/com/logicea/cards/CardRepository.java
+++ src/main/java/com/logicea/cards/CardRepository.java
@@ -1,4 +1,11 @@
 package com.logicea.cards;
+import com.logicea.cards.Card;
+import org.springframework.data.repository.CrudRepository;//i have to requests
 
-public class CardRepository {
+//repository is the way to interact with my base's data
+//i have access to basic methods such us findAll(),save(),delete()
+//Card is the class which is also the entity
+//Integer is the type of the primary key
+
+public interface  CardRepository extends CrudRepository<Card,Integer> {
 }
