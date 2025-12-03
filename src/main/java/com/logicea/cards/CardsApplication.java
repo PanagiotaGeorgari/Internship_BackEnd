b947d77d2756465d644e09364b39150e4cac2640
Panagiota4 <p.georgari@logicea.com>
2025-12-03 13:58:40 +0200

    comments

--- src/main/java/com/logicea/cards/CardsApplication.java
+++ src/main/java/com/logicea/cards/CardsApplication.java
@@ -1,13 +1,13 @@
 package com.logicea.cards;
-
+// 3/12 i put comments in order to run the DemoApplicationConnection for check the connection with sql 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class CardsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CardsApplication.class, args);
-	}
+	//public static void main(String[] args) {
+	//	SpringApplication.run(CardsApplication.class, args);
+	//}
 
 }
