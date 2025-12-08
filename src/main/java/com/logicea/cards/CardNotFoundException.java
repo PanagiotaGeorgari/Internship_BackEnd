46a64f7a0cffcd722954646704e8bf83ed0c54c7
Panagiota4 <p.georgari@logicea.com>
2025-12-08 13:58:38 +0200

    creation to handle get by id and id does not exist

--- src/main/java/com/logicea/cards/CardNotFoundException.java
+++ src/main/java/com/logicea/cards/CardNotFoundException.java
@@ -1,4 +1,9 @@
 package com.logicea.cards;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code=HttpStatus.NOT_FOUND,reason ="CardId Not Found")
 class CardNotFoundException extends RuntimeException {
      CardNotFoundException(int id) {
         super("Card with id " + id + " not found");
