fd7871f582bb3af88ba5f1e0146f1f3b8372b7f7
Panagiota4 <p.georgari@logicea.com>
2025-12-16 13:27:39 +0200

    add user in table

---
+++ src/main/resources/db/migration/V7__Add_User.sql
@@ -0,0 +1,12 @@
+INSERT INTO users (email,name,role,password)
+VALUES
+        ('u1@gmail.com', 'u1', 'ADMIN',534),
+        ('u2@gmail.com', 'u2', 'ADMIN',354),
+        ('u3@gmail.com', 'u3', 'ADMIN',867),
+        ('u4@gmail.com', 'u4', 'ADMIN',4345),
+        ('u5@gmail.com', 'u5', 'ADMIN',1687),
+        ('u6@gmail.com', 'u6', 'MEMBER',1057),
+        ('u7@gmail.com', 'u7', 'MEMBER',6894),
+        ('u8@gmail.com', 'u8', 'MEMBER',2879),
+        ('u9@gmail.com', 'u9', 'MEMBER',4223),
+        ('u10@gmail.com','u10', 'MEMBER',846);
