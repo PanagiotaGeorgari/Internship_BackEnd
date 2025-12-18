509cf6a0e6ea9bb9438e1736015004a548ea6f6c
Panagiota4 <p.georgari@logicea.com>
2025-12-18 13:49:57 +0200

    nothing changed

--- src/main/java/com/logicea/cards/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/logicea/cards/service/impl/UserInfoServiceImpl.java
@@ -1,4 +1,30 @@
 package com.logicea.cards.service.impl;
 
-public class UserInfoServiceImpl {
+import com.logicea.cards.dto.UserInfoDto;
+import com.logicea.cards.entity.UserInfo;
+import com.logicea.cards.mapper.UserInfoMapper;
+import com.logicea.cards.repository.UserInfoRepository;
+import com.logicea.cards.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserInfoServiceImpl implements UserInfoService {
+
+    private UserInfoRepository userInfoRepository;
+    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    public UserInfoServiceImpl(UserInfoRepository userInfoRepository, PasswordEncoder passwordEncoder) {
+        this.userInfoRepository = userInfoRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserInfoDto createUser(UserInfoDto userInfoDto) {
+        UserInfo userInfo= UserInfoMapper.toEntity(userInfoDto);
+        userInfo.setPassword(passwordEncoder.encode(userIn  fo.getPassword()));
+        userInfoRepository.save(userInfo);
+        return UserInfoMapper.toDto(userInfo);
+    }
 }
