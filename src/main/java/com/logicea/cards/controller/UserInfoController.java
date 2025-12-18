6d44fea16a9fb350e6285335873476c53c740d7c
Panagiota4 <p.georgari@logicea.com>
2025-12-18 14:01:16 +0200

    nothing changed

--- src/main/java/com/logicea/cards/controller/UserInfoController.java
+++ src/main/java/com/logicea/cards/controller/UserInfoController.java
@@ -1,4 +1,26 @@
 package com.logicea.cards.controller;
 
+import com.logicea.cards.dto.UserInfoDto;
+import com.logicea.cards.entity.UserInfo;
+import com.logicea.cards.service.UserInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user-info")
 public class UserInfoController {
+    @Autowired
+    UserInfoService userInfoService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> createUserInfo(@RequestBody UserInfoDto userInfoDto) {
+        UserInfoDto userInfoDto1= userInfoService.createUser(userInfoDto);
+        return new ResponseEntity<>("User "+userInfoDto1.email()+
+                "is registered successfully", HttpStatus.CREATED);
+    }
 }
