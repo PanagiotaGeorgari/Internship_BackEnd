c438f0b15643099d4b64588cf401a576f0965338
Panagiota4 <p.georgari@logicea.com>
2025-12-18 13:51:39 +0200

    creation

--- src/main/java/com/logicea/cards/service/CardService.java
+++ src/main/java/com/logicea/cards/service/CardService.java
@@ -1,4 +1,26 @@
 package com.logicea.cards.service;
 
+import com.logicea.cards.CardNotFoundException;
+import com.logicea.cards.PaginationResponse;
+import com.logicea.cards.dto.CardDto;
+import com.logicea.cards.entity.Card;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+
 public interface CardService {
+
+    List<CardDto> getAll();
+    CardDto getById(@PathVariable int cardId) throws CardNotFoundException ;
+    CardDto newCard(@Valid @RequestBody CardDto newCard);
+    CardDto replaceCard(@Valid @RequestBody CardDto newCard, @PathVariable int id) throws CardNotFoundException;
+    CardDto partialUpdateCard(@Valid @RequestBody CardDto updates, @PathVariable int id) throws CardNotFoundException;
+    void deleteCard(@PathVariable int id) throws CardNotFoundException;
+    PaginationResponse<CardDto> getCardsPagination(@RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "5") int size, @RequestParam(name = "sort", defaultValue = "cardId") String sort);
+
+
 }
