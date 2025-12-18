439dd19c95e22a6c26056dbc399eecb9fa3b113a
Panagiota4 <p.georgari@logicea.com>
2025-12-18 14:00:46 +0200

    changes after service layer

--- src/main/java/com/logicea/cards/controller/CardController.java
+++ src/main/java/com/logicea/cards/controller/CardController.java
@@ -1,106 +1,67 @@
-package com.logicea.cards;
+package com.logicea.cards.controller;
 
+import com.logicea.cards.CardNotFoundException;
+import com.logicea.cards.PaginationResponse;
+import com.logicea.cards.dto.CardDto;
+import com.logicea.cards.service.CardService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.security.access.prepost.PreAuthorize;
+
 import org.springframework.web.bind.annotation.*;
 
-@RestController // i prefer to return data (JSON) rather than HTML
-@RequestMapping("/api/cards") // in which path is my base and i can do my  requests
+import java.util.List;
 
+@RestController
+@RequestMapping("/api/cards")
 public class CardController {
-    @Autowired
-    private final CardRepository repository; //is the interface between my base and my backend
-                                            //via this i have access to my base
 
-    CardController(CardRepository repository) {
-        this.repository = repository;
+    private final CardService cardService;
+
+    // Dependency Injection μέσω Constructor
+    public CardController(CardService cardService) {
+        this.cardService = cardService;
     }
 
-    @GetMapping             //HTTP GET requests in RESTful web services
-                            // It simplifies mapping URLs to specific controller methods
-    @PreAuthorize("hasRole('ADMIN')")
-    public Iterable<Card> getAll() {
-        return repository.findAll();
+    @GetMapping
+
+    public List<CardDto> getAll() {
+        return cardService.getAll();
     }
 
-    @GetMapping("/{cardId}")//@PathVariable we gte the cardId from the url path of the request
-    @PreAuthorize("hasRole('USER')")
-    Card getById(@PathVariable int cardId) throws CardNotFoundException {
-        return repository.findById(cardId).orElseThrow(() -> new CardNotFoundException(cardId));
+    @GetMapping("/{cardId}")
+
+    public CardDto getById(@PathVariable int cardId) throws CardNotFoundException {
+        return cardService.getById(cardId);
     }
 
     @PostMapping
-    @PreAuthorize("hasRole('ADMIN')")
-    Card newCard(@Valid @RequestBody Card newCard) {
-        return repository.save(newCard);
+
+    public CardDto newCard(@Valid @RequestBody CardDto newCardDto) {
+        return cardService.newCard(newCardDto);
     }
 
     @PutMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
-    Card replaceCard(@Valid @RequestBody Card newCard, @PathVariable int id) throws CardNotFoundException {
-        return repository.findById(id).map(card -> {
-            card.setCardId(newCard.getCardId());
-            card.setName(newCard.getName());
-            card.setDescription(newCard.getDescription());
-            card.setColor(newCard.getColor());
-            card.setStatus(newCard.getStatus());
-            card.setCreatedBy(newCard.getCreatedBy());
-            return repository.save(newCard);
-        }).orElseThrow(() -> new CardNotFoundException(id));
+
+    public CardDto replaceCard(@Valid @RequestBody CardDto newCardDto, @PathVariable int id) throws CardNotFoundException {
+        return cardService.replaceCard(newCardDto, id);
     }
 
     @PatchMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
-    Card partialUpdateCard(@Valid @RequestBody Card updates, @PathVariable int id) throws CardNotFoundException {
-        return repository.findById(id).map(card -> {
-            if (updates.getName() != null) {
-                card.setName(updates.getName());
-            }
-            if (updates.getDescription() != null) {
-                card.setDescription(updates.getDescription());
-            }
-            if (updates.getColor() != null) {
-                card.setColor(updates.getColor());
-            }
-            if (updates.getStatus() != null) {
-                card.setStatus(updates.getStatus());
-            }
-            if (updates.getCreatedBy() != 0) {
-                card.setCreatedBy(updates.getCreatedBy());
-            }
 
-            return repository.save(card);
-        }).orElseThrow(() -> new CardNotFoundException(id));
+    public CardDto partialUpdateCard(@Valid @RequestBody CardDto updates, @PathVariable int id) throws CardNotFoundException {
+        return cardService.partialUpdateCard(updates, id);
     }
 
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
-    void deleteCard(@PathVariable int id) throws CardNotFoundException {
-        if(repository.findById(id).isPresent()) {
-            repository.deleteById(id);
-        }
-        else {
-            throw new CardNotFoundException(id);
-        }
-    }
-    /*@GetMapping("/{offset}/{pagesize}/{field}")
-    public Iterable<Card> getCardsPagenation(@PathVariable int offset, @PathVariable int pagesize, @PathVariable String field) {
-        Pageable pageable = PageRequest.of(offset, pagesize).withSort(Sort.by(Sort.Direction.ASC, field));
-        return repository.findAll(pageable);
-    }*/
-    @GetMapping("/")
-    public PaginationResponse<Card> getCardsPagination(@RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "5") int size,@RequestParam(name = "sort", defaultValue = "cardId") String sort)
-        {
-        // @RequestParam takes the value from the link
-        // @RequestParam if the value of the variable is missing then defaults with a sensible value
-        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.ASC, sort));
-        Page<Card> result = repository.findAll(pageable);
-        return new PaginationResponse<>(page,size,sort,result.getTotalPages(),result.getContent());
+
+    public void deleteCard(@PathVariable int id) throws CardNotFoundException {
+        cardService.deleteCard(id);
     }
 
+    @GetMapping("/page")
+    public PaginationResponse<CardDto> getCardsPagination(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "5") int size,
+            @RequestParam(name = "sort", defaultValue = "cardId") String sort) {
+        return cardService.getCardsPagination(page, size, sort);
+    }
 }
