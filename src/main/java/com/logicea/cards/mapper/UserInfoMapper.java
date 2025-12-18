09fb53362f947f1a7a36e060de27eb3e810632e4
Panagiota4 <p.georgari@logicea.com>
2025-12-18 13:56:04 +0200

    changed getters

--- src/main/java/com/logicea/cards/mapper/UserInfoMapper.java
+++ src/main/java/com/logicea/cards/mapper/UserInfoMapper.java
@@ -1,4 +1,15 @@
 package com.logicea.cards.mapper;
 
+import com.logicea.cards.dto.UserInfoDto;
+import com.logicea.cards.entity.UserInfo;
+
 public class UserInfoMapper {
+    public static UserInfoDto toDto (UserInfo userInfo) {
+        return new UserInfoDto(userInfo.getUserId(), userInfo.getEmail(), userInfo.getName(),
+                userInfo.getRole(), userInfo.getPassword());
+    }
+    public static UserInfo toEntity (UserInfoDto userInfoDto) {
+        return new UserInfo(userInfoDto.userId(), userInfoDto.email(), userInfoDto.name(),
+                userInfoDto.role(), userInfoDto.password());
+    }
 }
