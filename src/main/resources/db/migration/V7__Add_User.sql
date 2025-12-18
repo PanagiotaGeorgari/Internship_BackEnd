2aabd85717be1ac9d30ab8852eb66197d1e6c50f
Panagiota4 <p.georgari@logicea.com>
2025-12-18 13:46:08 +0200

    changed password to be encrypted

--- src/main/resources/db/migration/V7__Add_User.sql
+++ src/main/resources/db/migration/V7__Add_User.sql
@@ -1,12 +1,6 @@
 INSERT INTO users (email,name,role,password)
 VALUES
-        ('u1@gmail.com', 'u1', 'ADMIN',534),
-        ('u2@gmail.com', 'u2', 'ADMIN',354),
-        ('u3@gmail.com', 'u3', 'ADMIN',867),
-        ('u4@gmail.com', 'u4', 'ADMIN',4345),
-        ('u5@gmail.com', 'u5', 'ADMIN',1687),
-        ('u6@gmail.com', 'u6', 'MEMBER',1057),
-        ('u7@gmail.com', 'u7', 'MEMBER',6894),
-        ('u8@gmail.com', 'u8', 'MEMBER',2879),
-        ('u9@gmail.com', 'u9', 'MEMBER',4223),
-        ('u10@gmail.com','u10', 'MEMBER',846);
+        ('u1@gmail.com', 'u1', 'ADMIN','$2a$10$eA9LpX9Yp6UvX0.vY.fOueY7Iq.tO6Xo.LzS6qP5Y5Y5Y5Y5Y5Y5Y'),
+        ('u2@gmail.com', 'u2', 'ADMIN','$2a$10$8K0R0X9Yp6UvX0.vY.fOueY7Iq.tO6Xo.LzS6qP5Y5Y5Y5Y5Y5Y5Y'),
+        ('u3@gmail.com', 'u3', 'MEMBER','$2a$10$mR1S1X9Yp6UvX0.vY.fOueY7Iq.tO6Xo.LzS6qP5Y5Y5Y5Y5Y5Y5Y'),
+        ('u4@gmail.com', 'u4', 'MEMBER','$2a$10$pL2T2X9Yp6UvX0.vY.fOueY7Iq.tO6Xo.LzS6qP5Y5Y5Y5Y5Y5Y5Y');
