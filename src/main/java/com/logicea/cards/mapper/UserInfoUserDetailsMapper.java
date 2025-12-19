450ba6502af46c6d2e1bbc08ee49be8b2b3775a8
Panagiota4 <p.georgari@logicea.com>
2025-12-19 14:04:52 +0200

    create this class to take the details from the current user

--- src/main/java/com/logicea/cards/mapper/UserInfoUserDetailsMapper.java
+++ src/main/java/com/logicea/cards/mapper/UserInfoUserDetailsMapper.java
@@ -1,4 +1,62 @@
 package com.logicea.cards.mapper;
 
-public class UserInfoUserDetailsMapper {
+import com.logicea.cards.entity.UserInfo;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserInfoUserDetailsMapper implements UserDetails {
+   private int userId;
+    private String userName;
+   private String email;
+   private String password;
+   private String role;
+
+   public UserInfoUserDetailsMapper (UserInfo userInfo) {
+       this.userName = userInfo.getName();
+       this.email = userInfo.getEmail();
+       this.password = userInfo.getPassword();
+       this.role=userInfo.getRole().toString();
+       this.userId=userInfo.getUserId();
+
+   }
+    public int getUserId() { return userId; }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.toUpperCase()));
+    }
+
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
