4567760f25f5f3daa3c3f3b9feac51a1175b3ee9
Panagiota4 <p.georgari@logicea.com>
2025-12-19 14:11:03 +0200

    create class with method searchbyname

--- src/main/java/com/logicea/cards/service/impl/UserInfoUserDetailsServiceImpl.java
+++ src/main/java/com/logicea/cards/service/impl/UserInfoUserDetailsServiceImpl.java
@@ -1,4 +1,26 @@
 package com.logicea.cards.service.impl;
 
-public class UserInfoUserDetailsServiceImpl {
+import com.logicea.cards.entity.UserInfo;
+import com.logicea.cards.mapper.UserInfoUserDetailsMapper;
+import com.logicea.cards.repository.UserInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+
+public class UserInfoUserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserInfoRepository userInfoRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<UserInfo> userInfo=userInfoRepository.findByEmail(email);
+        return userInfo.map(UserInfoUserDetailsMapper::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User "+ email+" not found"));
+    }
+
 }
