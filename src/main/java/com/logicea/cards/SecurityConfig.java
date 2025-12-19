0ff2c5805f3bd567cd95530defb8d7e23d9e701e
Panagiota4 <p.georgari@logicea.com>
2025-12-19 13:56:22 +0200

    remove a pointer from permitAll

--- src/main/java/com/logicea/cards/SecurityConfig.java
+++ src/main/java/com/logicea/cards/SecurityConfig.java
@@ -1,15 +1,19 @@
 package com.logicea.cards;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -41,7 +45,8 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager(userDetailsOne, userDetailsTwo, admin);
 
     }*/
-
+    @Autowired
+    private UserDetailsService userDetailsService;
    @Bean
    public PasswordEncoder passwordEncoder() {return new BCryptPasswordEncoder();}
 
@@ -52,7 +57,7 @@ public class SecurityConfig {
         httpSecurity
                 .csrf(csrfCustomizer -> csrfCustomizer.disable())
                 .securityMatchers(matchers -> matchers
-                        .requestMatchers("/api/cards/**","/api/cardswelcome", "/user-info/register")
+                        .requestMatchers("/api/cardswelcome", "/user-info/register")
                 )
                 .authorizeHttpRequests(request ->
                         request.anyRequest().permitAll() // Permit all requests that match the matchers above
@@ -73,4 +78,5 @@ public class SecurityConfig {
                 );
         return httpSecurity.build();
     }
+
 }
