001bb9e87588b80da1ebcffcaac87eb544b1547b
Panagiota4 <p.georgari@logicea.com>
2025-12-22 13:58:27 +0200

    rename file

--- src/main/java/com/logicea/cards/dto/UserDto.java
+++ src/main/java/com/logicea/cards/dto/UserDto.java
@@ -2,7 +2,7 @@ package com.logicea.cards.dto;
 
 import com.logicea.cards.enums.UserRole;
 
-public record UserInfoDto(int userId, String email, String name,
-                          UserRole role, String password) {
+public record UserDto(int userId, String email, String name,
+                      UserRole role, String password) {
 
 }
