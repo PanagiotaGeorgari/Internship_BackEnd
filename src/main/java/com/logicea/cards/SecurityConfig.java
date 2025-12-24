ca95460348776d557580704e16d68fc020c61cf0
Panagiota4 <p.georgari@logicea.com>
2025-12-24 13:26:42 +0200

    now i have 1 filter chain and add the new filter for authentication and authorization

--- src/main/java/com/logicea/cards/SecurityConfig.java
+++ src/main/java/com/logicea/cards/SecurityConfig.java
@@ -1,11 +1,9 @@
 package com.logicea.cards;
 
-
+import com.logicea.cards.security.TokenAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,63 +11,40 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.Base64;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-
-
-
 public class SecurityConfig {
-    /*@Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails userDetailsOne = User.withUsername("User1")
-                .password(passwordEncoder().encode("pass1"))
-                .roles("MEMBER").build();
-        UserDetails userDetailsTwo = User.withUsername("User2")
-                .password(passwordEncoder().encode("pass2"))
-                .roles("MEMBER").build();
-        UserDetails admin = User.withUsername("Admin")
-                .password(passwordEncoder().encode("Admin1"))
-                .roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(userDetailsOne, userDetailsTwo, admin);
 
-    }*/
     @Autowired
     private UserDetailsService userDetailsService;
-   @Bean
-   public PasswordEncoder passwordEncoder() {return new BCryptPasswordEncoder();}
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 
     @Bean
-    @Order(0) // This filter chain has higher priority
-    public SecurityFilterChain publicFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf(csrfCustomizer -> csrfCustomizer.disable())
-                .securityMatchers(matchers -> matchers
-                        .requestMatchers( "/user-info/register")
-                )
-                .authorizeHttpRequests(request ->
-                        request.anyRequest().permitAll() // Permit all requests that match the matchers above
-                );
-        return httpSecurity.build();
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter(userDetailsService, passwordEncoder());
     }
-    // Filter chain for all other authenticated endpoints
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .csrf(csrfCustomizer -> csrfCustomizer.disable())
-                .authorizeHttpRequests(request ->
-                        request.anyRequest().authenticated() // All other requests must be authenticated
+                .csrf(csrf -> csrf.disable()) // disable for cookies
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/user-info/register", "/user-info/token").permitAll()//fr this urls there is not any authentication
+                        .anyRequest().authenticated() // any other request needs authentication
                 )
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                .addFilterBefore(tokenAuthenticationFilter(), //before any other filter we use tokenAuthenticationFilter()
+                        UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 
---
+++ src/main/java/com/logicea/cards/security/TokenAuthenticationFilter.java
@@ -0,0 +1,4 @@
+package com.logicea.cards.security;
+
+public class TokenAuthenticationFilter {
+}
