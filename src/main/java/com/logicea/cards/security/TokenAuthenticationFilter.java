e19fd9f1f30b01b596c7da9d345be5fe68545c9e
Panagiota4 <p.georgari@logicea.com>
2025-12-24 13:29:17 +0200

    new filter

--- src/main/java/com/logicea/cards/security/TokenAuthenticationFilter.java
+++ src/main/java/com/logicea/cards/security/TokenAuthenticationFilter.java
@@ -1,4 +1,51 @@
 package com.logicea.cards.security;
 
-public class TokenAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public TokenAuthenticationFilter(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = request.getHeader("Authorization");
+
+        if (token != null && !token.isEmpty()) {
+
+                String decoded = new String(Base64.getDecoder().decode(token));//decode the token
+                String[] token_decoded = decoded.split(":");
+
+
+                if (token_decoded.length == 2) { // we have 2 parts email:password
+                    String email = token_decoded[0];
+                    String rawPassword = token_decoded[1];
+
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(email); //load user from the base
+
+                    if (passwordEncoder.matches(rawPassword, userDetails.getPassword())) { // check if password matches with the encoded password in the base
+                        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                        SecurityContextHolder.getContext().setAuthentication(auth); // updates spring that user is authenticated
+                    }
+                }
+        }
+        filterChain.doFilter(request, response);
+    }
 }
