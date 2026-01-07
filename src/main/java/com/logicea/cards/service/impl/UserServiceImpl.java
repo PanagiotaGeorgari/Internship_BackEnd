3bf42b04d6f6ec7c22d0a580f9cabe55f88802c1
Panagiota4 <p.georgari@logicea.com>
2026-01-07 13:24:39 +0200

    changes after merge UserDetailsServiceImpl into UserServiceImpl

--- src/main/java/com/logicea/cards/service/impl/UserServiceImpl.java
+++ src/main/java/com/logicea/cards/service/impl/UserServiceImpl.java
@@ -6,11 +6,14 @@ import com.logicea.cards.mapper.UserMapper;
 import com.logicea.cards.repository.UserRepository;
 import com.logicea.cards.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
@@ -26,5 +29,10 @@ public class UserServiceImpl implements UserService {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return UserMapper.toDto(user);
+    }
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email: " + email + " not found"));
     }
 }
