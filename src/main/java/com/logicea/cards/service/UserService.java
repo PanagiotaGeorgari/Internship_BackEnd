07a7d1e616f865a3f8a90301bdd05c3285cddd2a
Panagiota4 <p.georgari@logicea.com>
2026-01-07 14:03:49 +0200

    now return User not UserDto

--- src/main/java/com/logicea/cards/service/UserService.java
+++ src/main/java/com/logicea/cards/service/UserService.java
@@ -1,7 +1,8 @@
 package com.logicea.cards.service;
 
 import com.logicea.cards.dto.UserDto;
+import com.logicea.cards.entity.User;
 
 public interface UserService {
-    public UserDto createUser(UserDto userDto);
+    public User createUser(User user);
 }
