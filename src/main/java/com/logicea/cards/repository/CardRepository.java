3e67f84760c1db18d01082305203891b8be5fc6b
Panagiota4 <p.georgari@logicea.com>
2026-01-07 13:46:47 +0200

    add a new method findByCreatedBy

--- src/main/java/com/logicea/cards/repository/CardRepository.java
+++ src/main/java/com/logicea/cards/repository/CardRepository.java
@@ -2,11 +2,13 @@ package com.logicea.cards.repository;
 import com.logicea.cards.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 //repository is the way to interact with my base's data
 //i have access to basic methods such us findAll(),save(),delete()
 //Card is the class which is also the entity
 //Integer is the type of the primary key
 
 public interface  CardRepository extends JpaRepository<Card,Integer> {
-
+    List<Card> findByCreatedBy(int userId);
 }
