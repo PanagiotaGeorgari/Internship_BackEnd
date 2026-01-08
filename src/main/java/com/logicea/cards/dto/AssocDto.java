6a273fd41b69279c460f39177abbf0ff9c5db366
Panagiota4 <p.georgari@logicea.com>
2026-01-08 13:25:28 +0200

    initial

--- src/main/java/com/logicea/cards/dto/AssocDto.java
+++ src/main/java/com/logicea/cards/dto/AssocDto.java
@@ -1,11 +1,11 @@
 package com.logicea.cards.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.logicea.cards.enums.Assoc;
+import com.logicea.cards.enums.AssocType;
 
 public record AssocDto(
         Integer id,
         @JsonProperty("lcardId") int lcardId,
-        @JsonProperty("assoc") Assoc assoc,
+        @JsonProperty("assoc") AssocType assoc,
         @JsonProperty("rcardId") int rcardId
 ) {}
