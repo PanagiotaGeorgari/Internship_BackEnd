47b00c45ef94499941588394845e74fc7dbf1a87
Panagiota4 <p.georgari@logicea.com>
2026-01-09 13:00:26 +0200

    change type of variable to string

--- src/main/java/com/logicea/cards/AssocNotFoundException.java
+++ src/main/java/com/logicea/cards/AssocNotFoundException.java
@@ -3,13 +3,8 @@ package com.logicea.cards;
 
 public//set custom HTTP status codes for controller methods
 class AssocNotFoundException extends RuntimeException {
-    public AssocNotFoundException(int id) {
+    public AssocNotFoundException(String id) {
         super("Asooc with id " + id + " not found");
     }
-
-    public static class AssocAlreadyExistsException extends RuntimeException {
-        public AssocAlreadyExistsException(String message) {
-            super(message);
-        }
-    }
+    
 }
