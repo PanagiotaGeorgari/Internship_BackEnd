9944e5024e331d667390a48752fad700aa424ba6
Panagiota4 <p.georgari@logicea.com>
2026-01-09 13:03:48 +0200

    add new methods to unsure unique assoc

--- src/main/java/com/logicea/cards/repository/AssocRepository.java
+++ src/main/java/com/logicea/cards/repository/AssocRepository.java
@@ -1,13 +1,13 @@
 package com.logicea.cards.repository;
 
 import com.logicea.cards.entity.Assoc;
+import com.logicea.cards.enums.AssocType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AssocRepository extends JpaRepository<Assoc,Integer> {
-
-    Optional<Assoc> findByRcardId(Integer integer);
-
-    Optional <Assoc> findByLcardId(Integer integer);
+    Optional<Assoc> findByLcardIdAndRcardIdAndAssoc(Integer lcardId, Integer rcardId, AssocType assocType);
+    Optional<Assoc> findByRcardIdAndLcardId(Integer rcardId, Integer lcardId);
 }
