1c0611363f1f48eb09c05b1e071fca72d04370d4
Panagiota4 <p.georgari@logicea.com>
2026-01-12 13:41:30 +0200

    add ne methods

--- src/main/java/com/logicea/cards/repository/AssocRepository.java
+++ src/main/java/com/logicea/cards/repository/AssocRepository.java
@@ -10,4 +10,8 @@ import java.util.Optional;
 public interface AssocRepository extends JpaRepository<Assoc,Integer> {
     Optional<Assoc> findByLcardIdAndRcardIdAndAssoc(Integer lcardId, Integer rcardId, AssocType assocType);
     Optional<Assoc> findByRcardIdAndLcardId(Integer rcardId, Integer lcardId);
+    List<Assoc> findByRcardId(Integer rcardId);
+    List<Assoc> findByLcardId(Integer lcardId);
+    List<Assoc> findAssocByAssocType(AssocType assocType);
+
 }
