e69e532dce7ff82cf0c66346818f8f2ae70980b9
Panagiota4 <p.georgari@logicea.com>
2026-01-14 13:50:39 +0200

    format

--- src/main/java/com/logicea/cards/controller/AssocController.java
+++ src/main/java/com/logicea/cards/controller/AssocController.java
@@ -1,10 +1,7 @@
 package com.logicea.cards.controller;
 
 import com.logicea.cards.dto.AssocDto;
-import com.logicea.cards.dto.CardDto;
 import com.logicea.cards.service.AssocService;
-import com.logicea.cards.service.CardService;
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -21,12 +18,14 @@ public class AssocController {
     public AssocController(AssocService assoc) {
         this.assocService = assoc;
     }
+
     @PostMapping("/card-assocs")
     @PreAuthorize("hasAnyRole('ADMIN','MEMBER')")
     public ResponseEntity<Map<String, List<Integer>>> createAssoc(@RequestBody AssocDto assocDto) throws AccessDeniedException {
         List<Integer> ids = assocService.newAssoc(assocDto);
         return ResponseEntity.ok(Map.of("ids", ids));
     }
+
     @DeleteMapping("/card-assocs/{id}")
     @PreAuthorize("hasAnyRole('ADMIN','MEMBER')")
     public void deleteAssoc(@PathVariable int id) throws AccessDeniedException {
